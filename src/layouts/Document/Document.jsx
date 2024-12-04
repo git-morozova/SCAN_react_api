14ca4efa8cdfb1a345164a70f3e22c49acee8baf
Alina Morozova <morozova.freelance@yandex.ru>
2024-12-04 21:28:53 +0300

    documents parser text ready

--- src/layouts/Document/Document.jsx
+++ src/layouts/Document/Document.jsx
@@ -2,7 +2,6 @@ import "./Document.css";
 import Button from "@/components/Button/Button";
 import { Link } from "react-router-dom";
 
-
 import XMLParser from 'react-xml-parser';
 
 import { Context } from "@/app";
@@ -35,21 +34,25 @@ const Document = () => {
     let url = item.url;     
 
     
- 
-
-
     //Парсер текста статьи
 
-    let text = [];
+    let rawText = item.content.markup
+    console.log(item.content.markup)
+
+    //убираем лишние теги
+    rawText = rawText.replace(/<entity.*?>/ig,'')
+    rawText = rawText.replace(/<\/entity>/ig,'')
+    rawText = rawText.replace(/<speech.*?>/ig,'')
+    rawText = rawText.replace(/<\/speech>/ig,'')
 
     //считаем количество текстовых нод в статье 
     let xmlCountNodes = new XMLParser().parseFromString(item.content.markup,"text/xml").getElementsByTagName("sentence")
-
+    
+    // в этот массив будем отбирать и добавлять предложения по тегу sentence
+    let text = [];    
+     
     for (let i = 0; i < xmlCountNodes.length; i++) {    
-      let xml = new XMLParser().parseFromString(item.content.markup,"text/xml").getElementsByTagName("sentence")[i]; 
-      console.log(xml);
-
-
+      let xml = new XMLParser().parseFromString(rawText,"text/xml").getElementsByTagName("sentence")[i]; 
 
       //функция замыкания
       function parseString (xmlRaw) {
@@ -59,45 +62,82 @@ const Document = () => {
           if(!xmlRaw.value) {            
             xml = xmlRaw.children[i]
             return parseString (xml)
-          }      else {
-            xml = String(xml.value);
-            xml = xml.replace(/<[^>]*>/g, '') //убираем лишние теги
-            text.push(xml);
+          } else {
+
+            //приведем строку в нужный вид
+
+            xml = String(xml.value);            
+            xml = xml.replace(/&lt;/g, '<') //переделываем спецсимволы в < и >
+            xml = xml.replace(/&gt;/g, '>') 
+
+            //убираем лишние теги - все нельзя, чтобы оставить абзацы
+            xml = xml.replace(/<blockquote.*?>/ig,'') 
+            xml = xml.replace(/<\/blockquote>/ig,'') 
+            xml = xml.replace(/<body.*?>/ig,'') 
+            xml = xml.replace(/<\/body>/ig,'')
+            xml = xml.replace(/<b.*?>/ig,'')
+            xml = xml.replace(/<\/b>/ig,'')
+            xml = xml.replace(/<em.*?>/ig,'')
+            xml = xml.replace(/<\/em>/ig,'')
+            xml = xml.replace(/<i.*?>/ig,'')
+            xml = xml.replace(/<\/i>/ig,'')
+            xml = xml.replace(/<strong.*?>/ig,'')
+            xml = xml.replace(/<\/strong>/ig,'')
+            xml = xml.replace(/<span.*?>/ig,'')
+            xml = xml.replace(/<\/span>/ig,'')
+            xml = xml.replace(/<div.*?>/ig,'')
+            xml = xml.replace(/<\/div>/ig,'')
+            xml = xml.replace(/<data.*?>/ig,'')
+            xml = xml.replace(/<\/data>/ig,'')
+            xml = xml.replace(/<a.*?>/ig,'')
+            xml = xml.replace(/<\/a>/ig,'')
+            xml = xml.replace(/<index.*?>/ig,'')
+            xml = xml.replace(/<\/index>/ig,'')
+            xml = xml.replace(/<small.*?>/ig,'')
+            xml = xml.replace(/<\/small>/ig,'')
+            xml = xml.replace(/<article.*?>/ig,'')
+            xml = xml.replace(/<\/article>/ig,'')
+            xml = xml.replace(/<ul.*?>/ig,'')
+            xml = xml.replace(/<\/ul>/ig,'')
+            xml = xml.replace(/<hr.*?>/ig,'')
+            xml = xml.replace(/<li.*?>/ig,'')
+            xml = xml.replace(/<header.*?>/ig,'')
+            xml = xml.replace(/<\/header>/ig,'')
+
+            xml = xml.replace(/<source.*?>/ig,'')
+            xml = xml.replace(/<\/source>/ig,'')            
+            xml = xml.replace(/<picture.*?>/ig,'')
+            xml = xml.replace(/<\/picture>/ig,'')
+
+            //тут только закрывающие
+            xml = xml.replace(/<\/p>/ig,'')
+            xml = xml.replace(/<\/h1>/ig,'')
+            xml = xml.replace(/<\/h2>/ig,'')
+            xml = xml.replace(/<\/h3>/ig,'')
+            xml = xml.replace(/<\/h4>/ig,'')
+
+            //вместо открывающего тега <p> пишем br, чтобы потом разделить абзацы
+            xml = xml.replace(/<p.*?>/ig,'<br>')    
+            xml = xml.replace(/<h1.*?>/ig,'<br>') 
+            xml = xml.replace(/<h2.*?>/ig,'<br>') 
+            xml = xml.replace(/<h3.*?>/ig,'<br>') 
+            xml = xml.replace(/<h4.*?>/ig,'<br>') 
+            xml = xml.replace(/<\/li>/ig,'<br>')           
+
+            //добавляем получившуюся строку в конец массива
+            text.push(xml + " ");
           }
         }        
       }
       parseString(xml)
-
     }
 
+    //объединим элементы массива в единый элемент, если нет тега <br>
+    text = text.join("")
+    text = text.split('<br> ')
 
-/*
-  <?xml version="1.0" encoding="utf-8"?>
-  <scandoc>
-    <sentence>
-      <speech author-local-id="1">
-        <speech author-local-id="5">
-          <speech author-local-id="6">
-            По всей <entity type="location" local-id="29">России </entity>температура в декабре 
-            ожидается около или выше нормы</speech></speech></speech>, заявил научный руководитель 
-            <entity type="company" local-id="5">Гидрометцентра РФ </entity>
-            <entity type="person" local-id="1">Роман Вильфанд</entity>. 
-            </sentence>
-            <sentence>По <entity type="person" local-id="1">его </entity>словам, 
-            <speech author-local-id="1"><speech author-local-id="5">
-              <speech author-local-id="6">нет ни одного региона, где в следующем месяце температура
-                 прогнозировалась бы ниже нормальной.</speech></speech></speech> </sentence>
-                 <sentence>Синоптик подробно рассказал, где в декабре будет теплее обычного, 
-                  а также поделился детальным прогнозом по количеству осадков.
-                  &lt;/div&gt;&lt;div&gt;&lt;p&gt;</sentence>
-                  <sentence>
-                    <entity type="theme" local-id="19">Во всех <entity type="location" local-id="29">
-                      российских </entity>регионах температура воздуха в декабре ожидается около 
-                      или выше нормы. </entity></sentence>
-                      <sentence><entity type="theme" local-id="24">
-                        Подробный прогноз погоды на первый зимний месяц сделал научный руководитель <entity type="company" local-id="6">Гидрометцентра России </entity><entity type="person" local-id="1">Роман Вильфанд </entity>в беседе с <entity type="company" local-id="3">агентством Интерфакс</entity>.&lt;/p&gt;&lt;blockquote&gt;&lt;p&gt;</entity></sentence><sentence>"На всей территории страны в декабре нет регионов, где прогнозировался бы класс температуры ниже нормы. </sentence><sentence>Везде около и выше нормы, но при этом нечасто встречающиеся ситуации, когда на большей части территории страны температура прогнозируется выше нормы", – рассказал синоптик.&lt;/p&gt;&lt;/blockquote&gt;&lt;p&gt;</sentence><sentence>По его словам, превышение среднемесячной температуры ожидается на восточной половине европейской части <entity type="location" local-id="29">России</entity>, на востоке Северо-Западного федерального округа, востоке Центрального федерального округа, во всем Приволжском федеральном округе, на Урале, а также на большей части Сибирского федерального округа и северной половине Дальневосточного федерального округа.&lt;/p&gt;&lt;p&gt;</sentence><sentence>На оставшейся европейской части <entity type="location" local-id="29">России </entity>температура будет около нормы. </sentence><sentence>В частности, речь идет о <entity type="location" local-id="30">Москве</entity>, <entity type="location" local-id="31">Санкт-Петербурге</entity>, Центральном Черноземье и юге Европейской <entity type="location" local-id="29">России </entity>– Северо-Кавказском и Южном федеральных округах.&lt;/p&gt;&lt;p&gt;</sentence><sentence>Кроме того, близкая к норме температура в декабре прогнозируется на юге и юго-востоке Сибири, в южной половине <entity type="location" local-id="32">Якутии</entity>, <entity type="location" local-id="33">Амурской области</entity>, на юге <entity type="location" local-id="34">Хабаровского края </entity>и в <entity type="location" local-id="35">Приморье</entity>.&lt;/p&gt;&lt;blockquote&gt;&lt;p&gt;</sentence><sentence><speech author-local-id="1"><speech author-local-id="5"><speech author-local-id="6">"Это связано с тем, что прогнозируется частое вторжение (воздушных масс с. – &lt;em&gt;RT&lt;/em&gt;) Атлантики в азиатскую часть страны, что редко бывает"</speech></speech></speech>, – объяснил <entity type="person" local-id="1">Вильфанд</entity>.&lt;/p&gt;&lt;/blockquote&gt;&lt;p&gt;</sentence><sentence><entity type="theme" local-id="11"><entity type="theme" local-id="12"><entity type="theme" local-id="13"><entity type="theme" local-id="14"><entity type="theme" local-id="15"><entity type="theme" local-id="16"><entity type="theme" local-id="17">Согласно прогнозу синоптика, на европейской территории страны в большинстве регионов выпадет нормальное количество осадков, однако на юге Центрального федерального округа, в <entity type="location" local-id="37">Рязанской</entity>, <entity type="location" local-id="38">Воронежской</entity>, <entity type="location" local-id="39">Тульской</entity>, <entity type="location" local-id="40">Липецкой </entity>и <entity type="location" local-id="36">Тамбовской областях </entity>будет наблюдаться их дефицит. </entity></entity></entity></entity></entity></entity></entity></sentence><sentence><entity type="theme" local-id="18">Недостаток снега также ожидается в северной половине <entity type="location" local-id="32">Якутии</entity>.&lt;/p&gt;&lt;p&gt;</entity></sentence><sentence>В то же время избыток осадков в декабре прогнозируется в <entity type="location" local-id="41">Ненецком автономном округе</entity>, в <entity type="location" local-id="43">Архангельской </entity>и <entity type="location" local-id="42">Мурманской областях</entity>, <entity type="location" local-id="44">Республике Коми</entity>, а также в северной половине Уральского федерального округа, в <entity type="location" local-id="45">Пермском крае</entity>, <entity type="location" local-id="47">Ямало-Ненецком </entity>и <entity type="location" local-id="46">Ханты-Мансийском автономных округах</entity>.&lt;/p&gt;&lt;p&gt;</sentence><sentence>Кроме того, снегом завалит юго-восток Сибири, в том числе <entity type="location" local-id="48">Красноярский край</entity>, <entity type="location" local-id="49">Иркутскую область </entity>и <entity type="location" local-id="50">Туву</entity>, а также Дальний Восток – <entity type="location" local-id="51">Бурятию</entity>, Чукотку, <entity type="location" local-id="52">Магаданскую область </entity>и <entity type="location" local-id="35">Приморский край</entity>.&lt;/p&gt;&lt;p&gt;</sentence><sentence><entity type="theme" local-id="25">Между тем специалист <entity type="company" local-id="4">информационного агентства "Метеоновости" </entity><entity type="person" local-id="2">Татьяна Позднякова </entity>в беседе с RT рассказала о погоде в столице в предстоящие выходные. </entity></sentence><sentence><entity type="theme" local-id="26">По <entity type="person" local-id="2">ее </entity>словам, 30 ноября и 1 декабря погоду будет определять гребень антициклона с запада.&lt;/p&gt;&lt;p&gt;</entity></sentence><sentence><speech author-local-id="2"><speech author-local-id="4">"Существенных осадков у нас не ожидается. </speech><sentence>Днем мы рассчитываем, что будут появляться просветы. </sentence><sentence>В ночные часы у нас температура слабо отрицательная, где-то до -1...-3 °C, а дневная температура останется около нуля. </sentence><sentence><entity type="theme" local-id="27">То есть погода, в принципе, довольно комфортная"</entity></sentence>, – отметила <entity type="person" local-id="2">Позднякова</entity>.&lt;/p&gt;&lt;p&gt;</speech></sentence><sentence><entity type="person" local-id="2">Она </entity>предупредила, что <speech author-local-id="2"><speech author-local-id="4">местами в <entity type="location" local-id="30">Москве </entity>будет формироваться гололедица – там, где будет подтаивать, а потом замерзать недавно выпавший снег.</speech></speech>&lt;/p&gt; &lt;p&gt;</sentence><sentence><entity type="theme" local-id="22">В начале декабря положительная температурная аномалия на территории Московского региона сохранится, утверждает синоптик.&lt;/p&gt;&lt;p&gt;</entity></sentence><sentence><entity type="theme" local-id="28">"На следующей неделе у нас сохранится теплая погода с аномалией температуры выше нормы на 3-4 °C. </entity></sentence><sentence>Также около нуля в дневные часы и слабо отрицательная температура ночью. </sentence><sentence>И осадки наиболее вероятны в конце первой пятидневки. </sentence><sentence>Пока (предварительный прогноз. – &lt;em&gt;RT&lt;/em&gt;) на 4-5 декабря – небольшой снег. </sentence><sentence>А уже затем ожидается понижение температуры. </sentence><sentence>То есть, скорее всего, вторая пятидневка в декабре будет уже с легким морозом и в ночное, и в дневное время", – заключила собеседница RT.&lt;/p&gt;&lt;p&gt;</sentence><sentence><entity type="theme" local-id="23"><speech author-local-id="0"><speech author-local-id="5"><speech author-local-id="6">Ранее о положительной погодной аномалии RT также рассказал замначальника <entity type="company" local-id="6"><entity type="company" local-id="7">ситуационного центра Гидрометцентра России </entity></entity><entity type="person" local-id="0">Анатолий Цыганков</entity>.</speech></speech></speech> &lt;/p&gt;&lt;p&gt;</entity></sentence><sentence><entity type="theme" local-id="20"><entity type="theme" local-id="21">Между тем сообщалось, что метеорологи <entity type="location" local-id="47">Ямало-Ненецкого автономного округа </entity>зафиксировали новый температурный рекорд в <entity type="location" local-id="53">Салехарде </entity>– впервые за 142 года метеонаблюдений столбики термометров 27 ноября показали +2,9 °С.&lt;/p&gt;&lt;/div&gt;&lt;/body&gt; &lt;/data&gt; </entity></entity></sentence></scandoc>
+    //конец парсера
 
-*/
 
     docsResultArray.push({
       "id": key, 
@@ -117,6 +157,19 @@ const Document = () => {
     <div className="flex documents flex-btw">
       
       {docsResultArray.map((item) => {
+        
+        //оборачиваем абзацы в p
+        let text = [];
+        item.text.forEach((p, index) => {
+          p = p.replace(/<br.*?>/ig,'')
+          p = p.replace(/<sentence.*?>/ig,'')
+          p = p.replace(/<\/sentence>/ig,'')
+          text.push(
+            <p key={index} className="documents__p">              
+              {p}
+            </p>
+          );          
+        });  
 
         return (
           <div className="documents__card" key={item.id}>
@@ -136,7 +189,7 @@ const Document = () => {
               className="stretch documents__img"
             />
 
-            <div className="documents__text grey">{item.text}</div>
+            <div className="documents__text grey">{text}</div>
 
             <div className="flex documents__bottom">
               <Link to={item.link} target="_blank">
